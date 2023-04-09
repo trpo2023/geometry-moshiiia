dc7bf8582a67644661aa9cc45615e811f5168946
root <mivk17@gmail.com>
2023-04-09 22:33:36 +0700

    Fix format

--- src/geometry/Circle.h
+++ src/geometry/Circle.h
@@ -3,27 +3,25 @@
 #include <iostream>
 
 // Point structure
-struct Point
-{
+struct Point {
     double x;
     double y;
 };
 
 // Circle structure
-struct Circle
-{
+struct Circle {
     Point center;
     double radius;
     double area;
     double perimeter;
 };
 
-
 // Regular expressions for each object type
 const std::regex symbols("\\(.*\\)$");
 const std::regex circle_regex("circle$");
-const std::regex circle_regex_num("\\s*(\\d+\\.?\\d*).\\s*(\\d+\\.?\\d*),\\s*(\\d+\\.?\\d*)$");
+const std::regex circle_regex_num(
+        "\\s*(\\d+\\.?\\d*).\\s*(\\d+\\.?\\d*),\\s*(\\d+\\.?\\d*)$");
 
-bool validateCircle(const std::string &input, Circle &circle);
+bool validateCircle(const std::string& input, Circle& circle);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,33 @@
+#include "geometry/Circle.h"
+#include <bits/stdc++.h>
+#include <iostream>
 #include <regex>
 #include <string.h>
 #include <vector>
-#include <iostream>
-#include <bits/stdc++.h>
-#include "geometry/Circle.h"
-
 
 // Main function to test the input validator
 int main()
 {
-    std::cout << "Input params.\nExample:circle(10.0 2.0, 3)\n 0-Exit" << std::endl;
+    std::cout << "Input params.\nExample:circle(10.0 2.0, 3)\n 0-Exit"
+              << std::endl;
     std::string input;
-     while (input != "0")
-     {
+    while (input != "0") {
         std::cout << "Input params.\n" << std::endl;
-        getline(std::cin,input);
+        getline(std::cin, input);
         Circle circle;
-        if (validateCircle(input, circle))
-        {
-            //площадь и периметр
-            //std::cout << "Valid circle: center=(" << std::setprecision(3)<<circle.center.x << "," << std::setprecision(3)<<circle.center.y << "), radius=" << std::setprecision(3)<<circle.radius << std::endl;
-            std::cout << "Valid circle: area = "<< std::setprecision(3)<<circle.area << ", perimeter = " << std::setprecision(3)<<circle.perimeter << "." << std::endl;
-        }
-        else
-        {
+        if (validateCircle(input, circle)) {
+            // площадь и периметр
+            // std::cout << "Valid circle: center=(" <<
+            // std::setprecision(3)<<circle.center.x << "," <<
+            // std::setprecision(3)<<circle.center.y << "), radius=" <<
+            // std::setprecision(3)<<circle.radius << std::endl;
+            std::cout << "Valid circle: area = " << std::setprecision(3)
+                      << circle.area << ", perimeter = " << std::setprecision(3)
+                      << circle.perimeter << "." << std::endl;
+        } else {
             std::cout << "Invalid input" << std::endl;
         }
-        std::cout<<std::endl;
-     }
+        std::cout << std::endl;
+    }
     return 0;
 }
