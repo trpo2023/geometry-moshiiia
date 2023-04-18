65e87349a7173a1bd83204e35ce97db5a3043fb1
moshiiia <mivk17@gmail.com>
2023-04-18 16:31:57 +0700

    Create array of figure

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,26 @@
 // Main function to test the input validator
 int main()
 {
+    int n = 0;
+    std::cout << "Input count of figures:";
+    std:: cin >> n;
+    Circle *arr = new Circle[n];
+    
     std::cout << "Input params.\nExample:circle(10.0 2.0, 3)\n 0-Exit"
               << std::endl;
-    std::string input;
-    while (input != "0") {
+    
+    for (int i=0; i < n; i++){
         std::cout << "Input params.\n" << std::endl;
+        std::string input; 
         getline(std::cin, input);
-        Circle circle;
-        if (validateCircle(input, circle)) {
-            // площадь и периметр
-            // std::cout << "Valid circle: center=(" <<
-            // std::setprecision(3)<<circle.center.x << "," <<
-            // std::setprecision(3)<<circle.center.y << "), radius=" <<
-            // std::setprecision(3)<<circle.radius << std::endl;
+        if (validateCircle(input, arr[i])) {
             std::cout << "Valid circle: area = " << std::setprecision(3)
-                      << circle.area << ", perimeter = " << std::setprecision(3)
-                      << circle.perimeter << "." << std::endl;
+                      << arr[i].area << ", perimeter = " << std::setprecision(3)
+                      << arr[i].perimeter << "." << std::endl;
         } else {
             std::cout << "Invalid input" << std::endl;
         }
         std::cout << std::endl;
-    }
+     }
     return 0;
 }
